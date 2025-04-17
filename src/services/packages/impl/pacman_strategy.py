c46a4ac5540186ded370dd1b4983ee466f582b71
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-04-17 17:40:02 -0300

    Agregar estrategias de gestión de paquetes para apt, yum, dnf y pacman, incluyendo métodos para listar paquetes actualizables y actualizar todos los paquetes.

--- src/services/packages/impl/pacman_strategy.py
+++ src/services/packages/impl/pacman_strategy.py
@@ -3,8 +3,8 @@ from ..package_manager_strategy import PackageManagerStrategy
 
 class PacmanStrategy(PackageManagerStrategy):
     def list_upgradable_packages(self):
-        result = subprocess.run(['pacman', '-Qu'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(['checkupdates'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return result.stdout.decode('utf-8').splitlines()
 
     def update_all_packages(self):
-        subprocess.run(['pacman', '-Syu']) 
+        subprocess.run(['run0','pacman', '-Syu']) 
